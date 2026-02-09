a079e44c3f83e59d5a58118cc85c6813c5ca2c1f
Tyler Kuhn <contact@tyknet.xyz>
2026-02-08 22:49:02 -0500

    Improved Assembly Resolver
    Use enumerables to allow lazy/asynchronous fetching.
    Compare assembly names and versions more directly in case the assembly filename does not match expectations.

--- AtomicModManager/PluginResolver.cs
+++ AtomicModManager/PluginResolver.cs
@@ -2,28 +2,68 @@
 
 namespace AtomicFramework
 {
-    internal class PluginResolver(string bep_path, string managed_path): MetadataAssemblyResolver
+    internal class PluginResolver: MetadataAssemblyResolver
     {
-        private readonly string bep_path = bep_path;
-        private readonly string managed_path = managed_path;
+        private readonly string bep_path;
+        private readonly string managed_path;
 
-        public override Assembly? Resolve(MetadataLoadContext context, AssemblyName assemblyName)
+        internal PluginResolver(string bep_path, string managed_path)
         {
-            Console.WriteLine($"Resolving {assemblyName.Name}");
+            this.bep_path = bep_path;
+            this.managed_path = managed_path;
+        }
 
-            string[] plugins = Directory.GetFiles(Path.Combine(bep_path, "plugins"), assemblyName.Name + ".dll", SearchOption.AllDirectories);
-            if (plugins.Length > 0)
-                return context.LoadFromAssemblyPath(plugins[0]);
+        public override Assembly? Resolve(MetadataLoadContext ctx, AssemblyName target)
+        {
+            Console.WriteLine($"Resolving {target.Name}");
 
-            plugins = Directory.GetFiles(Path.Combine(bep_path, "core"), assemblyName.Name + ".dll");
-            if (plugins.Length > 0)
-                return context.LoadFromAssemblyPath(plugins[0]);
+            IEnumerable<string> plugins = GetAssemblies();
+            foreach (string plugin in plugins)
+            {
+                try
+                {
+                    AssemblyName potential = AssemblyName.GetAssemblyName(plugin);
 
-            plugins = Directory.GetFiles(managed_path, assemblyName.Name + ".dll");
-            if (plugins.Length > 0)
-                return context.LoadFromAssemblyPath(plugins[0]);
+                    if (CompareNames(target, potential))
+                        return ctx.LoadFromAssemblyPath(plugin);
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine(e);
+                }
+            }
 
             return null;
+        }
+
+        private IEnumerable<string> GetAssemblies()
+        {
+            IEnumerable<string> plugins = Directory.EnumerateFiles(Path.Combine(bep_path, "plugins"), "*.dll", SearchOption.AllDirectories);
+            foreach (string plugin in plugins)
+                yield return plugin;
+
+            plugins = Directory.EnumerateFiles(Path.Combine(bep_path, "core"), "*.dll", SearchOption.AllDirectories);
+            foreach (string plugin in plugins)
+                yield return plugin;
+
+            plugins = Directory.EnumerateFiles(managed_path, "*.dll", SearchOption.AllDirectories);
+            foreach (string plugin in plugins)
+                yield return plugin;
+        }
+
+        private static bool CompareNames(AssemblyName target, AssemblyName sample)
+        {
+            if (target.Version == null ||
+                (target.Version.Major <= 0 && target.Version.Minor <= 0 &&
+                target.Version.Revision <= 0 && target.Version.Build <= 0))
+            {
+                return target.Name == sample.Name;
+            }
+            else
+            {
+                return target.Name == sample.Name &&
+                    (target.Version.Major == sample.Version!.Major && target.Version.Minor <= sample.Version.Minor);
+            }
         }
     }
 }
